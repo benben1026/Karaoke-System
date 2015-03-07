89b411cddd13ec3ee309465695824aaf0f4af272
Elvin Jin <elvinjin8@gmail.com>
2015-03-08 01:10:42 +0800

    Add playing function, pause function and stop function

--- P2P Karaoke System/P2P Karaoke System/MainWindow.xaml.cs
+++ P2P Karaoke System/P2P Karaoke System/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,6 +21,8 @@ namespace P2P_Karaoke_System
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool playing = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,6 +30,41 @@ namespace P2P_Karaoke_System
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
+            if (!playing)
+                mediaElement1.Play();
+            else
+                mediaElement1.Pause();
+
+            playing = !playing;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Stop();
+            slider1.Value = 0;
+            playing = false;
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            slider1.Value = mediaElement1.Position.TotalSeconds;
+        }
+
+        private void load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.AddExtension = true;
+            dialog.DefaultExt = "*.*";
+            dialog.Filter = "Media Files (*.*)|*.*";
+            dialog.ShowDialog();
+
+            try { mediaElement1.Source = new Uri(dialog.FileName); }
+            catch { new NullReferenceException("Error"); }
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(timer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
         }
     }
 }
