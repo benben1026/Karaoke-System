12cdd0aa89955892354414daa376ee7113957d1d
Elvin Jin <elvinjin8@gmail.com>
2015-03-13 19:05:38 +0800

    Finish WavStream.cs subclass implementation

--- P2P Karaoke System/P2P Karaoke System/WavStream.cs
+++ P2P Karaoke System/P2P Karaoke System/WavStream.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace P2P_Karaoke_System
 {
@@ -7,8 +8,9 @@ namespace P2P_Karaoke_System
     {
         private Stream stream;
         private long length;
-        private long position;
+        private long dataPosition;
         private WavFormat format;
+        private object lockObject;
 
         public WavFormat Format
         {
@@ -18,72 +20,145 @@ namespace P2P_Karaoke_System
             }
         }
 
+        private string ReadChunk(BinaryReader reader)
+        {
+            byte[] ch = new byte[4];
+            reader.Read(ch, 0, ch.Length);
+            return Encoding.ASCII.GetString(ch);
+        }
+
         public void ReadHeader()
         {
+            BinaryReader Reader = new BinaryReader(stream);
+            if (ReadChunk(Reader) != "RIFF")
+                throw new InvalidDataException("Invalid file format");
 
+            Reader.ReadInt32(); // Get rid of the first 8 bytes of "RIFF" info
+
+            if (ReadChunk(Reader) != "WAVE")
+                throw new InvalidDataException("Invalid file format");
+
+            if (ReadChunk(Reader) != "fmt ")
+                throw new InvalidDataException("Invalid file format");
+
+            int len = Reader.ReadInt32();
+            if (len < 16) // bad format chunk length
+                throw new InvalidDataException("Invalid file format");
+
+            format = new WavFormat(22050, 16, 2); // initialize to any format
+            format.wFormatTag = Reader.ReadInt16();
+            format.nChannels = Reader.ReadInt16();
+            format.nSamplesPerSec = Reader.ReadInt32();
+            format.nAvgBytesPerSec = Reader.ReadInt32();
+            format.nBlockAlign = Reader.ReadInt16();
+            format.wBitsPerSample = Reader.ReadInt16();
+
+            // advance in the stream to skip the wave format block
+            for (len -= 16; len > 0; --len)
+            {
+                Reader.ReadByte();
+            }
+
+            // assume the data chunk is aligned
+            while (stream.Position < stream.Length && ReadChunk(Reader) != "data");
+
+            if (stream.Position >= stream.Length)
+                throw new InvalidDataException("Invalid file format");
+
+            length = Reader.ReadInt32();
+            dataPosition = stream.Position;
+
+            Position = 0;
         }
 
         public WavStream(string fileName)
         {
+            lockObject = new object();
             stream = new FileStream(fileName, FileMode.Open);
             ReadHeader();
         }
 
+        ~WavStream()
+        {
+            Dispose(false);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && stream != null)
+            {
+                stream.Close();
+                stream = null;
+            }
+            base.Dispose(disposing);
+        }
+
         public override void Write(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException();
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            int read;
+            lock (lockObject) {
+                int lengthToRead = (int)Math.Min(count, length - Position);
+                read = stream.Read(buffer, offset, lengthToRead);
+            }
+            return read;
         }
 
         public override void SetLength(long value)
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException();
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            throw new NotImplementedException();
+            lock (lockObject)
+            {
+                switch (origin)
+                {
+                    case SeekOrigin.Begin:
+                        stream.Position = offset + dataPosition;
+                        break;
+                    case SeekOrigin.Current:
+                        stream.Seek(offset, SeekOrigin.Current);
+                        break;
+                    case SeekOrigin.End:
+                        stream.Position = dataPosition + length - offset;
+                        break;
+                }
+                return Position;
+            }
         }
 
-        public override void Flush()
-        {
-            throw new NotImplementedException();
-        }
+        public override void Flush() { }
 
         public override long Position
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            get { return stream.Position - dataPosition; }
+            set { Seek(value, SeekOrigin.Begin); }
         }
 
         public override long Length
         {
-            get { throw new NotImplementedException(); }
+            get { return length; }
         }
 
         public override bool CanWrite
         {
-            get { throw new NotImplementedException(); }
+            get { return false; }
         }
 
         public override bool CanSeek
         {
-            get { throw new NotImplementedException(); }
+            get { return true; }
         }
 
         public override bool CanRead
         {
-            get { throw new NotImplementedException(); }
+            get { return true; }
         }
     }
 }
