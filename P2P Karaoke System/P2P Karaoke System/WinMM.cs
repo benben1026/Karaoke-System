aa43bf0835f16302a0c65b0c8d3792a78319feab
Elvin Jin <elvinjin8@gmail.com>
2015-03-13 16:52:02 +0800

    Convert WinMM.dll to C# code

---
+++ P2P Karaoke System/P2P Karaoke System/WinMM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace P2P_Karaoke_System
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WavFormat
+    {
+        public short wFormatTag;
+        public short nChannels;
+        public int nSamplesPerSec;
+        public int nAvgBytesPerSec;
+        public short nBlockAlign;
+        public short wBitsPerSample;
+        public short cbSize;
+
+        public WavFormat(int sampleRate, int bits, int channels)
+        {
+            wFormatTag = (short)WavFormats.PCM;
+            nChannels = (short)channels;
+            nSamplesPerSec = sampleRate;
+            wBitsPerSample = (short)bits;
+            cbSize = 0;
+
+            nBlockAlign = (short)(channels * (bits / 8));
+            nAvgBytesPerSec = nSamplesPerSec * nBlockAlign;
+        }
+    }
+
+    public enum WavFormats
+    {
+        PCM = 1,
+        FLOAT = 3,
+    }
+
+    class WinMM
+    {
+    }
+}
