e74a021cc5aa1cb41619ea141d8cd28bd22ccadb
Elvin Jin <elvinjin8@gmail.com>
2015-03-13 16:51:46 +0800

    Add WavStream class inherit from Stream

--- P2P Karaoke System/P2P Karaoke System/WavStream.cs
+++ P2P Karaoke System/P2P Karaoke System/WavStream.cs
@@ -1,12 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace P2P_Karaoke_System
 {
-    class WavStream
+    class WavStream : Stream, IDisposable
     {
+        private Stream stream;
+        private long length;
+        private long position;
+        private WavFormat format;
+
+        public WavFormat Format
+        {
+            get
+            {
+                return format;
+            }
+        }
+
+        public void ReadHeader()
+        {
+
+        }
+
+        public WavStream(string fileName)
+        {
+            stream = new FileStream(fileName, FileMode.Open);
+            ReadHeader();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override long Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanWrite
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanSeek
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool CanRead
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
