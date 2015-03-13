dae7199c87ae2e788c43dd26d34177d2a0be6ae6
Elvin Jin <elvinjin8@gmail.com>
2015-03-13 16:53:47 +0800

    Add general audio player controller

---
+++ P2P Karaoke System/P2P Karaoke System/AudioPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2P_Karaoke_System
+{
+    class AudioPlayer
+    {
+        public void Play()
+        {
+
+        }
+
+        public void Pause()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public bool IsPlaying()
+        {
+            return false;
+        }
+    }
+
+}
