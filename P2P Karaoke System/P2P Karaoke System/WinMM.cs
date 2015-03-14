566745d0fd22a55bd27bb278bd0ccea2efb574ea
Elvin Jin <elvinjin8@gmail.com>
2015-03-14 10:00:04 +0800

    Add convertion from WinMM.dll to C#

--- P2P Karaoke System/P2P Karaoke System/WinMM.cs
+++ P2P Karaoke System/P2P Karaoke System/WinMM.cs
@@ -63,12 +63,27 @@ namespace P2P_Karaoke_System
 
     internal class Native
     {
+        public const int MMSYSERR_NOERROR = 0;
+        public const int CALLBACK_FUNCTION = 0x00030000;  
+
+        // callbacks
+        public delegate void WaveDelegate(IntPtr hdrvr, Messages uMsg, int dwUser, ref WaveHdr wavhdr, int dwParam2);
+
         private const string mmdll = "winmm.dll";
 
+        [DllImport(mmdll)]
+        public static extern int waveOutGetNumDevs();
         [DllImport(mmdll)]
         public static extern int waveOutPrepareHeader(IntPtr hWaveOut, ref WaveHdr lpWaveOutHdr, int uSize);
         [DllImport(mmdll)]
         public static extern int waveOutUnprepareHeader(IntPtr hWaveOut, ref WaveHdr lpWaveOutHdr, int uSize);
-        
+        [DllImport(mmdll)]
+        public static extern int waveOutWrite(IntPtr hWaveOut, ref WaveHdr lpWaveOutHdr, int uSize);
+        [DllImport(mmdll)]
+        public static extern int waveOutOpen(out IntPtr hWaveOut, int uDeviceID, ref WavFormat lpFormat, WaveDelegate dwCallback, int dwInstance, int dwFlags);
+        [DllImport(mmdll)]
+        public static extern int waveOutReset(IntPtr hWaveOut);
+        [DllImport(mmdll)]
+        public static extern int waveOutClose(IntPtr hWaveOut);
     }
 }
