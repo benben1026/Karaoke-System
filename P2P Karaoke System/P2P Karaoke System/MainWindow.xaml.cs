8fd07f7185fa0a97cfc2236da2ead92b6d9c5c4b
Elvin Jin <elvinjin8@gmail.com>
2015-03-14 22:29:47 +0800

    Solve image path

--- P2P Karaoke System/P2P Karaoke System/MainWindow.xaml.cs
+++ P2P Karaoke System/P2P Karaoke System/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,26 +20,21 @@ namespace P2P_Karaoke_System
     /// </summary>
     public partial class MainWindow : Window
     {
-        enum AudioFormat : int
-        {
-            AUDIOFORMAT_WAV = 0,
-            AUDIOFORMAT_MP3 = 1,
-            AUDIOFORMAT_MP4 = 2,
-            AUDIOFORMAT_WMA = 3,
-            AUDIOFORMAT_M4A = 4,
-        }
-
         private Microsoft.Win32.OpenFileDialog openDialog;
         private WavFormat format;
         private Stream audioStream;
         private WaveOutPlayer thePlayer;
+        private string audioFormat = null;
+        // For file format other than WAV
+        private NAudio.Wave.DirectSoundOut nAudioOutput = null;
+        private NAudio.Wave.BlockAlignReductionStream nAudioStream = null;
 
         public MainWindow()
         {
             InitializeComponent();
 
             this.openDialog = new Microsoft.Win32.OpenFileDialog();
-            this.openDialog.Filter = "Audio File (*.wav, *.mp3, *.mp4, *.wma, *.m4a)|*.wav;*.mp3;*.mp4;*.wma;*.m4a";
+            this.openDialog.Filter = "Audio File (*.wav, *.mp3, *.mp4, *.wma, *.m4a)|*.wav;*.mp3;*.mp4;*.wma;*.m4a;";
         }
 
         private void Filler(IntPtr data, int size)
@@ -114,30 +106,57 @@ namespace P2P_Karaoke_System
             }
         }
 
+        private void DisposeWave()
+        {
+            if (nAudioOutput != null)
+            {
+                if (nAudioOutput.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+                    nAudioOutput.Stop();
+                nAudioOutput = null;
+            }
+            if (nAudioStream != null)
+            {
+                nAudioStream.Dispose();
+                nAudioStream = null;
+            }
+        }
+
         private void load_Click(object sender, RoutedEventArgs e)
         {
             if (openDialog.ShowDialog() == true)
             {
                 CloseFile();
-                string ext = System.IO.Path.GetExtension(openDialog.FileName);
-                Console.WriteLine(ext);
+                DisposeWave();
+                audioFormat = System.IO.Path.GetExtension(openDialog.FileName);
+                Console.WriteLine(audioFormat);
 
-                try
+                if (audioFormat.Equals(".wav"))
                 {
-                    WavStream S = new WavStream(openDialog.FileName);
-                    if (S.Length <= 0)
-                        throw new Exception("Invalid WAV file");
-                    format = S.Format;
-                    Console.WriteLine(format);
-                    if (format.wFormatTag != (short)WavFormats.PCM && format.wFormatTag != (short)WavFormats.FLOAT)
-                        throw new Exception("Olny PCM files are supported");
+                    try
+                    {
+                        WavStream S = new WavStream(openDialog.FileName);
+                        if (S.Length <= 0)
+                            throw new Exception("Invalid WAV file");
+                        format = S.Format;
+                        Console.WriteLine(format);
+                        if (format.wFormatTag != (short)WavFormats.PCM && format.wFormatTag != (short)WavFormats.FLOAT)
+                            throw new Exception("Olny PCM files are supported");
 
-                    audioStream = S;
+                        audioStream = S;
+                    }
+                    catch (Exception err)
+                    {
+                        CloseFile();
+                        System.Windows.Forms.MessageBox.Show(err.Message);
+                    }
                 }
-                catch (Exception err)
+                else
                 {
-                    CloseFile();
-                    System.Windows.Forms.MessageBox.Show(err.Message);
+                    NAudio.Wave.WaveStream pcm = new NAudio.Wave.AudioFileReader(openDialog.FileName);
+                    nAudioStream = new NAudio.Wave.BlockAlignReductionStream(pcm);
+                    nAudioOutput = new NAudio.Wave.DirectSoundOut();
+                    nAudioOutput.Init(nAudioStream);
+                    nAudioOutput.Play();
                 }
             }
         }
