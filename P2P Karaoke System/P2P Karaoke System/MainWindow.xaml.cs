b66c905a76af4deea62ef8783a1ab9df9b908d15
zxmeng <bluedreamermzx@gmail.com>
2015-03-16 00:01:25 +0800

    tiny change

--- P2P Karaoke System/P2P Karaoke System/MainWindow.xaml.cs
+++ P2P Karaoke System/P2P Karaoke System/MainWindow.xaml.cs
@@ -96,6 +96,7 @@ namespace P2P_Karaoke_System
             isPlaying = false;
             if (audioStream != null) audioStream.Position = 0;
 
+            progressSlider.Maximum = 0;
             if (thePlayer != null)
             {
                 try { thePlayer.Dispose(); }
@@ -105,7 +106,7 @@ namespace P2P_Karaoke_System
 
         void timer_Tick(object sender, EventArgs e)
         {
-            progressSlider.Value = currentPosition();
+            progressSlider.Value =currentPosition();
         }
 
         public void CloseFile()
@@ -147,7 +148,6 @@ namespace P2P_Karaoke_System
                 DisposeWave();
                 audioFormat = System.IO.Path.GetExtension(openDialog.FileName);
                 Console.WriteLine(audioFormat);
-                progressSlider.Value = 0;
 
                 if (audioFormat.Equals(".wav"))
                 {
@@ -309,18 +309,6 @@ namespace P2P_Karaoke_System
                 {
                     MessageBox.Show("Datebase Connection Failure","Error",MessageBoxButton.OK, MessageBoxImage.Error);
                 }
-            }
-        }
-        private void progressSlider_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            if (audioStream != null)
-            {
-                Console.WriteLine((int)((Slider)sender).Value);
-                audioStream.Position = (int)((Slider)sender).Value * format.nAvgBytesPerSec;
-            }
-            else
-            {
-                audioStream.Position = 0;
             }
         }
     }
